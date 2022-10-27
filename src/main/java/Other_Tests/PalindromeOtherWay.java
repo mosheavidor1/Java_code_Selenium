3eb64164abe28e166c53aa8882d9c507f8f53212
mavidor <mavidor@fortinet.com>
2022-10-27 12:53:44 +0300

    PalindromeOtherWay

---
+++ src/main/java/Other_Tests/PalindromeOtherWay.java
@@ -0,0 +1,66 @@
+package Other_Tests;
+
+import io.netty.handler.ssl.JdkApplicationProtocolNegotiator;
+
+import java.util.Scanner;
+
+public class PalindromeOtherWay {
+    public static void main(String[] args) {
+        do {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a plaindrome word");
+            String pal = sc.nextLine();
+
+
+            if (GetPal(pal)) {
+                System.out.println("This is a palindrome word");
+            } else {
+                System.out.println("This is not a palindrome word");
+            }
+            System.out.println(" Do yo wish to continue");
+            {
+
+            }
+
+
+            if (sc.nextLine().equals("yes")) ;
+
+
+
+
+               else  {
+               sc.nextLine().equals("no");
+               System.out.println("Thank you for playing ");
+               break;
+
+               }
+
+
+
+        }
+
+        while (true);
+    }
+
+   public static boolean GetPal(String name ) {
+
+       String original = "";
+        original=name;
+
+       String reverse = "";
+
+       int length = original.length();
+       for (int i = length - 1; i >= 0; i--)
+           reverse = reverse+original.charAt(i);
+
+       if (original.equals(reverse)) {
+         return true;
+       } else {
+           return false;
+       }
+   }
+
+}
+
+

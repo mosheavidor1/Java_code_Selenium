46957837132e96faf1aa216c4e47a4ac5c621d6e
mavidor <mavidor@fortinet.com>
2022-11-03 15:35:16 +0200

    Pyramid

---
+++ src/main/java/Other_Tests/Loops_Practice.java
@@ -0,0 +1,25 @@
+package Other_Tests;
+
+public class Loops_Practice {
+
+    public static void main(String[] args) {
+        System.out.println(getThis());
+
+
+    }
+
+    public static  boolean getThis(){
+
+        int x=11;
+        for(int i=1;i<x;i++){
+            if(x<i){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return getThis();
+    }
+}
+
--- src/main/java/Other_Tests/Nested_basic_loop.java
+++ src/main/java/Other_Tests/Nested_basic_loop.java
@@ -1,13 +1,12 @@
 package Other_Tests;
 
-public class Pyramid {
+public class Nested_basic_loop {
     public static void main(String[] args) {
 
 
-        int x = 11;
-
-        for (int i = 1; i <x; i++) {
-            for (int j = 1; j <i; j++) {
+        int x = 10;
+        for (int i = 1; i < x; i++){
+            for(int j =1;j<i;j++){
                 System.out.print(j);
             }
             System.out.println(i);

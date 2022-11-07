098c56a4b786734b9524306c83ddaea8ee4c9b6f
mavidor <mavidor@fortinet.com>
2022-11-07 17:36:56 +0200

    Person

---
+++ src/main/java/Other_Tests/SwitchAndCase.java
@@ -0,0 +1,33 @@
+package Other_Tests;
+
+public class SwitchAndCase {
+
+    public static void main(String[] args) {
+
+        int x = 50;
+
+        for (int i = 0; i <= x; i++) {
+
+
+                if (x < 100) {
+
+                    switch (x) {
+                        case 50:
+                            System.out.println("hello");
+                            break;
+                        case 40:
+                            System.out.println("bye");
+                            break;
+
+                        default:
+                            System.out.println("WoW");
+                    }
+
+                } else if (x == 100) {
+                    System.out.println("moshe");
+                } else {
+                    System.out.println("Alex");
+                }
+            }
+        }
+    }

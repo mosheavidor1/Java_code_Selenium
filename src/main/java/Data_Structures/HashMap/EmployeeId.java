148420656e0953a200bf0f5d28e17293bef2c8b9
mavidor <mavidor@fortinet.com>
2022-11-08 16:01:46 +0200

    Person

---
+++ src/main/java/Data_Structures/HashMap/EmployeeId.java
@@ -0,0 +1,22 @@
+package Data_Structures.HashMap;
+
+import java.util.HashMap;
+
+public class EmployeeId {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> EMPI = new HashMap<>();
+
+        EMPI.put("Moshe", 10);
+        EMPI.put("Danny", 20);
+        EMPI.put("John", 70);
+        EMPI.put("Sam", 4);
+        EMPI.put("Guy", 35);
+        EMPI.put("Ben", 14);
+
+        System.out.println(EMPI);
+        System.out.println(EMPI.get("John"));
+    }
+}
+

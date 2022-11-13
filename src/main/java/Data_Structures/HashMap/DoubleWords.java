ce65562c9f4a56b8cbf767fd98a24a25d4eaa764
mavidor <mavidor@fortinet.com>
2022-11-13 17:53:25 +0200

    HashMap

---
+++ src/main/java/Data_Structures/HashMap/DoubleWords.java
@@ -0,0 +1,51 @@
+package Data_Structures.HashMap;
+
+import com.sun.source.tree.WhileLoopTree;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class DoubleWords {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        File F = new File("C:\\Users\\mavidor\\Desktop\\practice\\words.txt");
+        Scanner sc = new Scanner(F);
+        int count = 2;
+
+
+        Map<String, Integer> text = new HashMap<>();
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/Data_Structures/Set/SetPractice.java
@@ -0,0 +1,35 @@
+package Data_Structures.Set;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetPractice {
+
+
+    public static void main(String[] args) {
+
+
+        Set<Integer> numbers= new HashSet<Integer>();
+        Set<String> words= new TreeSet<>();
+
+
+
+        numbers.add(1);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(9);
+        numbers.addAll(Set.of(5,3,56,66));
+
+        words.add("moshe");
+        words.add("Alex");
+        words.add("77");
+
+
+
+        System.out.println(numbers);
+        System.out.println(words);
+
+
+    }
+}

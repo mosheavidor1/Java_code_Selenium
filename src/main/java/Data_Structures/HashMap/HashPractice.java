9f4a597cca51233fcf7571d3089c2bc37d07e46d
mavidor <mavidor@fortinet.com>
2022-11-14 20:07:51 +0200

    HashMap

---
+++ src/main/java/Data_Structures/HashMap/HashPractice.java
@@ -0,0 +1,26 @@
+package Data_Structures.HashMap;
+
+import java.util.*;
+
+public class HashPractice {
+
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = new HashMap<>();
+        {
+
+            map.put("moshe", 1234);
+            map.put("John", 555);
+            map.put("Sem", 44);
+            map.put("Bob", 7);
+            map.put("John", 39);
+
+            System.out.println(map);
+
+
+
+        }
+    }
+}
+

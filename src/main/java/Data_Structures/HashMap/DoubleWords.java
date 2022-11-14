3cecc8fe8a48996dd2cda4ba7616dda8ab83ab1b
mavidor <mavidor@fortinet.com>
2022-11-14 17:44:12 +0200

    HashMap

--- src/main/java/Data_Structures/HashMap/DoubleWords.java
+++ src/main/java/Data_Structures/HashMap/DoubleWords.java
@@ -1,51 +1,38 @@
 package Data_Structures.HashMap;
 
-import com.sun.source.tree.WhileLoopTree;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
-public class DoubleWords {
-
-
+public class DoubleWords{
     public static void main(String[] args) throws FileNotFoundException {
+        Double();
 
-
-        File F = new File("C:\\Users\\mavidor\\Desktop\\practice\\words.txt");
-        Scanner sc = new Scanner(F);
-        int count = 2;
-
-
-        Map<String, Integer> text = new HashMap<>();
-
-
-
-
-        }
     }
 
+    public static void Double() throws FileNotFoundException {
+        File path = new File("C:\\Practice\\DoubleWords.txt");
+        Scanner scan = new Scanner(path);
+        String text = scan.nextLine();
+        String words[]=text.split(" ");
+        Map<String,Integer> counter= new HashMap<String, Integer>();
+        for (int i=0;i<words.length;i++) {
+            if (counter.containsKey(words[i])) {
+                counter.put(words[i], counter.get(words[i]) + 1);
+            } else {
+                counter.put(words[i], 1);
+            }
+        }
+        Set<String> execute= counter.keySet();
+        for(int i=0;i<execute.size();i++){
+            if(counter.get(words[i])>1){
+                System.out.print(words[i]+": "+counter.get(words[i]));
 
+            }
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+}

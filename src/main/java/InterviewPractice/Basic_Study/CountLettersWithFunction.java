750207ef1f0eef058f75a7e3320759ed7e9ecca0
mavidor <mavidor@fortinet.com>
2022-11-20 15:09:12 +0200

    HashMap

---
+++ src/main/java/InterviewPractice/Basic_Study/CountLettersWithFunction.java
@@ -0,0 +1,30 @@
+package InterviewPractice.Basic_Study;
+
+public class CountLettersWithFunction {
+    public static void main(String[] args) {
+
+        String name = "Moshe Avidor";
+        char text[] = name.toCharArray();
+        System.out.println(GetLetters(text));
+
+    }
+
+    private static String GetLetters(char letters[]){
+
+        String sum="";
+        for(int i=0;i<letters.length;i++){
+
+            while (i<letters.length&&letters[i]!=' '){
+                sum=sum+letters[i];
+                i++;
+            }
+            if(sum.length()>0){
+                System.out.println(sum+"="+sum.length());
+                sum="";
+
+            }
+        }
+        return sum;
+
+    }
+}

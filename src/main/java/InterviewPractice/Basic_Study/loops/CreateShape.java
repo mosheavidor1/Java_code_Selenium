078585ed903b89fd98415eb660d3e8a865fca3c6
mavidor <mavidor@fortinet.com>
2022-11-26 16:09:26 +0200

    Shape

---
+++ src/main/java/InterviewPractice/Basic_Study/loops/CreateShape.java
@@ -0,0 +1,18 @@
+package InterviewPractice.Basic_Study.loops;
+
+public class CreateShape {
+
+    public int[][] pyramid(int i, int j) {
+
+        int arr[][] = new int[10][10];
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j <= i; j++) {
+                System.out.print("*" + " ");
+
+
+            }
+            System.out.println();
+        }
+        return arr;
+    }
+}

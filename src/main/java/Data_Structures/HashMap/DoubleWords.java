55c8143d9452f642713478ac5eab735c2fa7bf23
mosheavidor1 <47886429+mosheavidor1@users.noreply.github.com>
2022-12-15 14:19:40 +0200

    Pyramid sum

--- src/main/java/Data_Structures/HashMap/DoubleWords.java
+++ src/main/java/Data_Structures/HashMap/DoubleWords.java
@@ -1,39 +1,54 @@
+
 package Data_Structures.HashMap;
+
+import com.google.common.collect.Multiset;
+import org.checkerframework.checker.units.qual.K;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.security.Key;
+import java.security.KeyStore;
+import java.util.*;
 
-public class DoubleWords {
+public class DoubleWords
+
+{
+
     public static void main(String[] args) throws FileNotFoundException {
-        Double();
-
+        getDouble();
     }
 
-    public static void Double() throws FileNotFoundException {
-        File path = new File("C:\\Practice\\DoubleWords.txt");
-        Scanner scan = new Scanner(path);
-        String text = scan.nextLine();
-        String[] words = text.split(" ");
-        Map<String, Integer> counter = new HashMap<String, Integer>();
-        for (int i = 0; i < words.length; i++) {
-            if (counter.containsKey(words[i])) {
-                counter.put(words[i], counter.get(words[i]) + 1);
+    private static void getDouble() throws FileNotFoundException {
+
+        File path = new File("C:\\practice\\words.txt");
+
+        Scanner sc = new Scanner(path);
+
+        String words[] = sc.nextLine().split("\\W");
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            if (map.get(word) != null) {
+                map.put(word, map.get(word) + 1);
+
             } else {
-                counter.put(words[i], 1);
+                map.put(word, 1);
             }
         }
-        Set<String> execute = counter.keySet();
-
-        for (int i = 0; i < execute.size(); i++) {
-            if (counter.get(words[i]) > 1) {
-                System.out.print(words[i] + ": " + counter.get(words[i]));
-
-            }
+        Set<String> key = map.keySet();
+        for (String value : key) {
+            if (map.get(value) > 1)
+                System.out.println(value+":"+map.get(value));
         }
 
     }
 }
+
+
+
+
+
+
+
+
+

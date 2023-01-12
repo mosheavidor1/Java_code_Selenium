587a527ee74fcc143ec7caffe2a416c6576615cb
mosheavidor1 <47886429+mosheavidor1@users.noreply.github.com>
2023-01-12 22:16:10 +0200

    sum of two numbers

--- src/main/java/InterviewPractice/Basic_Study/SumOfArrays.java
+++ src/main/java/InterviewPractice/Basic_Study/SumOfArrays.java
@@ -5,41 +5,82 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+
 public class SumOfArrays {
-
-
     public static void main(String[] args) {
         int[] a = {2, 4};
-        int[] b = {3, 10};
-        System.out.println(Arrays.toString(Sum(a, b)));
+        int[] b = {5, 10};
 
-
+        //System.out.println(Arrays.toString(Sum(a, b)));
+        System.out.println(Sum(a,b));
     }
 
-
-    public static int[]Sum(int[] A, int[] B) {
-
-        int C[]=new int[2];
-
-
-        try {
-            for (int i = 0; i <= A.length; i++) {
-                C[i]= A[i] + B[i];
-                for (int j = 0; j <= B.length; j++) {
-                    C[j]=A[j]+B[j];
-                }
-
-            }
-
-
-        } catch (ArrayIndexOutOfBoundsException e) {
-
+    public static int Sum(int[] A, int[] B) {
+        int[] C = new int[2];
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i] + B[i];
         }
-        return C;
+        int sum = 0;
+        for (int i = 0; i < C.length; i++) {
+            sum += C[i];
+        }
+
+        return sum;
+
     }
 }
 
 
+//Second way - using J in the for loop :
 
-//Create the sum of 2 array A{}+B{}=C{} and the sum of all the numbers in c{};
-//For example - A{ 2,4} B{5,10} -> 2+5 , 4+10  C{7,14} -> C=21;
+
+//public class SumOfArrays {
+//
+//
+//    public static void main(String[] args) {
+//        int[] a = {2, 4};
+//        int[] b = {3, 10};
+//
+//
+//
+//        System.out.println(Arrays.toString(Sum(a, b)));
+//
+//
+//    }
+//
+//    public static int[] Sum(int[] A, int[] B) {
+//
+//        int[] C = new int[2];
+//
+//
+//        try {
+//            for (int i = 0; i <= A.length; i++) {
+//                C[i] = A[i] + B[i];
+//
+//                for (int j = 0; j <= B.length; j++) {
+//                    C[j] = A[j] + B[j];
+//
+//                }
+//            }
+//            int sum = 0;
+//            for (int i = 0; i <= C.length; i++) {
+//                sum += C[i];
+//            }
+//            System.out.println("Sum of the two arrays: " + sum);
+//            return C;
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//
+//        }
+//
+//return C;
+//    }
+//
+//}
+//
+//
+//
+//
+//
+//
+////Create the sum of 2 array A{}+B{}=C{} and the sum of all the numbers in c{};
+////For example - A{ 2,4} B{5,10} -> 2+5 , 4+10  C{7,14} -> C=21;

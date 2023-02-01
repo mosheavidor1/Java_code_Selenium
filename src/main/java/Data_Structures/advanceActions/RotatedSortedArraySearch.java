3eae14fc61af1719c0f565e88c1852334d7a84e9
mosheavidor1 <47886429+mosheavidor1@users.noreply.github.com>
2023-02-01 16:35:25 +0200

    Getters

---
+++ src/main/java/Data_Structures/advanceActions/RotatedSortedArraySearch.java
@@ -0,0 +1,16 @@
+package Data_Structures.advanceActions;
+
+public class RotatedSortedArraySearch {
+
+    public static void main(String[] args) {
+
+        int[] B={3,4,5,8,12,9};
+
+Search.findParameter(B,3);
+
+        System.out.println(Search.findParameter(B,4));
+
+
+
+    }
+}
